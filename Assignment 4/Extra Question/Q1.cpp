6368779fe140ff363f5e908c63d30e2a0bab4e82
KrishnaGautam3007 <123109248+KrishnaGautam3007@users.noreply.github.com>
2025-10-25 17:04:29 +0530

    Update Q1.cpp

--- Assignment 4/Extra Question/Q1.cpp
+++ Assignment 4/Extra Question/Q1.cpp
@@ -1,1 +1,22 @@
-
+#include <iostream>
+#include <queue>
+using namespace std;
+void binnum(int n){
+    queue<string> q;
+    q.push("1"); 
+    for(int i=1;i<=n;i++){
+    string s=q.front();
+    q.pop();
+    cout<<s;
+    if(i!=n)
+        cout<<",";
+    q.push(s+"0");
+    q.push(s+"1");
+    }
+    cout << endl;
+}
+int main() {
+    int n=2;
+    binnum(n);
+    return 0;
+}
