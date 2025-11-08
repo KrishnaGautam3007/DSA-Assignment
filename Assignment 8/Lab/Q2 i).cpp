90ae1f768c046a56d1919f3ef3656ba4166078aa
KrishnaGautam3007 <123109248+KrishnaGautam3007@users.noreply.github.com>
2025-11-08 10:31:32 +0530

    Update Q2 i).cpp

--- Assignment 8/Lab/Q2 i).cpp
+++ Assignment 8/Lab/Q2 i).cpp
@@ -1,5 +1,6 @@
 // Online C++ compiler to run C++ program online
 #include <iostream>
+#include <queue>
 using namespace std;
 class Node{
     public:
@@ -21,6 +22,27 @@ bool searchRecursive(Node*root,int key){
     }
     return (searchRecursive(root->right,key)||searchRecursive(root->left,key));
 }
+bool searchNonRecursive(Node*root,int key){
+    if(root==NULL){
+        return false;
+    }
+    queue<Node*>qu;
+    qu.push(root);
+    while(!qu.empty()){
+        Node*temp=qu.front();
+        qu.pop();
+        if(temp->data==key){
+            return true;
+        }
+        if(temp->left!=NULL){
+            qu.push(temp->left);
+        }
+        if(temp->right!=NULL){
+            qu.push(temp->right);
+        }
+    }
+    return false;
+}
 int main() {
     Node* root=new Node(0);
     root->right=new Node(1);
@@ -30,5 +52,6 @@ int main() {
     root->left->right=new Node(5);
     root->left->left=new Node(6);
     cout<<searchRecursive(root,8);
+    cout<<searchNonRecursive(root,5);
     return 0;
 }
